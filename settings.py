5ac45b747509c2af028fec1c3bdcc23bb07e0cd7
chmjkb <jakubchmura1607@gmail.com>
2021-11-06 14:00:53 +0100

    Settings of the game

---
+++ settings.py
@@ -0,0 +1,21 @@
+class Settings:
+    """Class designed to keep the settings of a game"""
+
+    def __init__(self):
+        """Initializing variables"""
+
+        # Screen settings
+        self.screen_width = 900
+        self.screen_height = 500
+
+        # Background color (olive ish)
+        self.bg_color = (75, 108, 0)
+        # Ship moving speed
+        self.ship_speed = 1.5
+
+        # Bullet-related settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (244, 108, 0)  # Orange
+        self.bullets_allowed = 3  # This allows the player to shoot only 3 bullets at a time
