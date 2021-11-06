e352953bf947bf96f3815cd4e5d886e065e216d6
chmjkb <jakubchmura1607@gmail.com>
2021-11-06 15:06:25 +0100

    Alien file

--- alien.py
+++ alien.py
@@ -5,13 +5,15 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """A class displaying a single alien"""
 
-    def __init(self, ai_game):
+    def __init__(self, ai_game):
         """Initializing the alien"""
+
         super().__init__()
         self.screen = ai_game.screen
 
         # Loading the alien image
         self.image = pygame.image.load('images/alien.png')
+        self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect()
 
         # Setting the alien location close to the left top corner of the screen.
