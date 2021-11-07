1e74e53a3ca80c81acb48972273cc60e697378ff
chmjkb <jakubchmura1607@gmail.com>
2021-11-08 00:19:27 +0100

    implementing new features

--- alien.py
+++ alien.py
@@ -33,6 +33,6 @@ class Alien(Sprite):
 
     def update(self):
         """Moving the alien to the right"""
-        
+
         self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
