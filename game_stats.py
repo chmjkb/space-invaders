ed021d2b92b896b377e33628cb92fe5bddd3e131
chmjkb <jakubchmura1607@gmail.com>
2021-11-08 00:19:58 +0100

    keeping the game stats

---
+++ game_stats.py
@@ -0,0 +1,13 @@
+class GameStats:
+    """Monitoring stats in the game"""
+
+    def __init__(self, ai_game):
+        """Initializing the variables"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+        self.game_active = True
+
+    def reset_stats(self):
+        """Initializing variables which could change during the game"""
+        self.ships_left = self.settings.ship_limit
+
